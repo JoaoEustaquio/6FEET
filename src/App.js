74d5e04f2ebb3eddeef1a5fbc333397f59b1b068
Joao_Godinho <git.qodeless@outlook.com>
2022-05-01 18:58:18 -0300

    React Init and Implement Navbar using Material UI

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './components/Navbar.tsx'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Navbar 
+        display='flex'
+      />
+        <p>teste</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
